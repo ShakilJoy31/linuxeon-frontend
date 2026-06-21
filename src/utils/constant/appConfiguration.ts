58ad8f029f28f038a7dbd1e5aad1c4edab5081e1
ShakilJoy31 <joy31a@gmail.com>
2026-06-21 15:33:12 +0600

    Done

--- src/utils/constant/appConfiguration.ts
+++ src/utils/constant/appConfiguration.ts
@@ -28,3 +28,5 @@ export const appConfiguration: IConfigurationProps = {
   progressMessage:
     "Thank you for your interest! 🚀 We're currently working on implementing this feature. Stay tuned, as we'll be activating it very soon!",
 };
+
+
